32b1c621b1b0bce0f0f2cb917573b321340be1b7
diego Henrique Facchinetti <dhfacchinetti@stefanini.com>
2022-07-06 20:48:40 -0300

    Initial commit

---
+++ 05_firstStepsInKotlinWithSpringBoot/rest-with-spring-boot-and-kotlin-dhf/src/main/kotlin/br/com/dhfacchinetti/restwithspringbootandkotlindhf/Startup.kt
@@ -0,0 +1,11 @@
+package br.com.dhfacchinetti.restwithspringbootandkotlindhf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootAndKotlinDhfApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootAndKotlinDhfApplication>(*args)
+}
